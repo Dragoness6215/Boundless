bc5235c5a8a87700868d7c553113d8ab04827730
TheCrazyDragonLady <65409156+Dragoness6215@users.noreply.github.com>
2022-10-04 16:09:27 -0400

    InitialPush, basic bot

---
+++ functions.py
@@ -0,0 +1,5 @@
+import discord
+
+#Called by Main for each call; named after it.
+def help():
+    return 'This will be a help message'
---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from os import environ
+from dotenv import load_dotenv
+
+from functions import *
+
+
+#Global Variables
+prefix="!"
+
+##Setting up hidden variables (we don't want people knowing the token)
+load_dotenv()
+token = environ["TOKEN"]
+
+##initializing the bot
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready(): 
+    ##prints to the terminal once the bot's online
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message): ##called whenever the bot regesters a message
+
+    if message.author == client.user: ##removes responding to boundless's own messages
+        return
+
+    print("Recieved: "+ message.content)
+
+    ## each of the commands
+    if message.content.startswith(prefix+'hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith(prefix+'help'):
+        await message.channel.send(help())
+
+## Starts the running
+client.run(token)
